0f1ef91a4acf63283e4d82b6629b25fc037c5f50
ZeGittyBeau <116084375+ZeGittyBeau@users.noreply.github.com>
2022-10-26 15:26:14 +0200

    Add files via upload
    Må først lage app/side så adde till disse med url og alt

---
+++ models_car.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Car(models.Model):
+    make = models.CharField(max_length=50)
+    carmodel = models.CharField(max_length=50)
+    year = models.CharField(max_lenght=50)
+    location = models.CharField(max_lenght=50)
+    status = models.CharField(max_lenght=50)
+
+
+    def __str__(self):
+        return self.make + ' ' + self.carmodel
